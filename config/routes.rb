c8741e008a0ab256174da84b83dd83842527ef9b
Taylor Ermolov <taylor@nermolov.com>
2024-11-24 14:22:28 -0500

    redirect / to /jewelries

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :studios, except: :show
   resources :jewelries, except: :show
 
+  get "/", to: redirect("/jewelries", status: 302)
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
